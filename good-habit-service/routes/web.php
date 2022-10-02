8fdbc9ac4d45d163f19c3475ee50ba6e34f77c90
nakaji <nakaji.m.0625@gmail.com>
2022-10-02 19:58:36 +0900

    全部コミット

--- good-habit-service/routes/web.php
+++ good-habit-service/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/summary', \App\Http\Controllers\Summmary\IndexController::class);
+
+
 Route::get('/', function () {
     return view('welcome');
 });
