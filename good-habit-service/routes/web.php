867b0715b1ac9c5bd459b9cf830ac3f37a3ecb6e
nakaji <nakaji.m.0625@gmail.com>
2022-10-02 20:03:19 +0900

    install breeze

--- good-habit-service/routes/web.php
+++ good-habit-service/routes/web.php
@@ -12,10 +12,13 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/summary', \App\Http\Controllers\Summmary\IndexController::class);
-
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
